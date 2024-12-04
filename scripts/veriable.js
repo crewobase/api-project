b3e4456a87460cf003392def147744288a12e0d7
crewObase <bebongachemobase@gmail.com>
2024-12-04 01:49:11 -0800

    My movies api

--- scripts/veriable.js
+++ scripts/veriable.js
@@ -1,4 +1,4 @@
-fetch(`https://www.swapi.tech/api/films`)
+fetch('https://www.swapi.tech/api/films')
   .then((res) => res.json())
   .then((data) => {
     console.log(data)
@@ -8,12 +8,12 @@ fetch(`https://www.swapi.tech/api/films`)
     const titleFour = document.getElementById('paragraph-four')
     const titleFive = document.getElementById('paragraph-five')
     const titleSix = document.getElementById('paragraph-six')
-  for (let index = 0; index < data.result.length; index++) {
-    titleOne.innerHTML = data.result[0].properties.title
-    titleTwo.innerHTML = data.result[1].properties.title
-    titleThree.innerHTML = data.result[2].properties.title
-    titleFour.innerHTML = data.result[3].properties.title
-    titleFive.innerHTML = data.result[4].properties.title
-    titleSix.innerHTML = data.result[5].properties.title
-  }
-})
+    for (let index = 0; index < data.result.length; index++) {
+      titleOne.innerHTML = data.result[0].properties.title
+      titleTwo.innerHTML = data.result[1].properties.title
+      titleThree.innerHTML = data.result[2].properties.title
+      titleFour.innerHTML = data.result[3].properties.title
+      titleFive.innerHTML = data.result[4].properties.title
+      titleSix.innerHTML = data.result[5].properties.title
+    }
+  })
